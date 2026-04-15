83434c9bab1d986442bb3bbd9671e41adf420fdc
Lev Rtutin <qqk8@ya.ru>
2026-04-16 01:35:10 +0300

    forceHttps

--- backend/laravel/app/Providers/AppServiceProvider.php
+++ backend/laravel/app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (env('APP_ENV', 'local') === 'production') {
+        if (env('APP_ENV') === 'production') {
             URL::forceHttps();
         }
         
