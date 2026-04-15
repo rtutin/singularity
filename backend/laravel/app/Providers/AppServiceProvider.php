6bd1b7fec45309f24029c9fa81f31d4c6f9e3a8b
Lev Rtutin <qqk8@ya.ru>
2026-04-16 01:38:26 +0300

    upd

--- backend/laravel/app/Providers/AppServiceProvider.php
+++ backend/laravel/app/Providers/AppServiceProvider.php
@@ -24,9 +24,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (env('APP_ENV') === 'production') {
+        // @TODO: debug later
+        // if (env('APP_ENV') === 'production') {
             URL::forceHttps();
-        }
+        // }
         
         $this->configureDefaults();
     }
