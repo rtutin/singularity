3f54c745823f3171d22b65c39f3d670b3bf52293
Lev Rtutin <qqk8@ya.ru>
2026-04-22 22:41:47 +0300

    distribute-tg location

--- scripts/python/distribute-tg.sh
+++ scripts/python/distribute-tg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd /home/lain/random/singularity/scripts/python
-source /home/lain/random/.venv/bin/activate
-export DB_PATH="/home/lain/random/singularity/backend/laravel/database/database.sqlite"
-export DEPLOYER_PK=$(grep DEPLOYER_PK /home/lain/random/singularity/hardhat/.env | cut -d= -f2)
+cd /root/singularity/scripts/python
+source /root/singularity/scripts/python/bin/activate
+export DB_PATH="/root/singularity/backend/laravel/database/database.sqlite"
+export DEPLOYER_PK=$(grep DEPLOYER_PK /root/singularity/scripts/python/.env | cut -d= -f2)
 python distribute_tg.py
